1519458995c8334e19fb9396e91c9bbd8c9b306f
Andrew Woan <andrewwoan@gmail.com>
2026-01-12 15:17:56 -0500

    update: basic model loaded and animation playing test completedd

--- src/components/scene.js
+++ src/components/scene.js
@@ -1,15 +1,37 @@
 "use client";
-import { CameraControls } from "@react-three/drei";
+import {
+  CameraControls,
+  Environment,
+  PerspectiveCamera,
+} from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import Avatar from "./Avatar";
+import { useRef } from "react";
 
 const Scene = () => {
+  const camera = useRef();
+
   return (
     <Canvas>
+      <PerspectiveCamera
+        ref={camera}
+        makeDefault
+        fov={35}
+        position={[0, 2.5, -10]}
+      />
+
       <CameraControls />
-      <mesh>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshNormalMaterial />
-      </mesh>
+      <color attach="background" args={["#424242"]} />
+      <Environment
+        background={false}
+        environmentIntensity={1.5}
+        environmentRotation={[0, Math.PI / 2, 0]}
+        preset="city"
+      />
+
+      {/* <directionalLight intensity={2.5} /> */}
+      <ambientLight intensity={0.25} />
+      <Avatar />
     </Canvas>
   );
 };
