7d3910998ee7991ffc60fbf1b8d20f0a79df0e0b
Andrew Woan <andrewwoan@gmail.com>
2026-01-18 10:33:33 -0500

    removeable item feature

--- src/components/ui/AssetsBox/AssetsBox.jsx
+++ src/components/ui/AssetsBox/AssetsBox.jsx
@@ -37,6 +37,28 @@ const AssetsBox = () => {
           );
         })}
 
+        {currentCategory?.optional && (
+          <button
+            onClick={() => changeAsset(currentCategory.name, null)}
+            className="asset-button"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18 18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        )}
+
         {currentCategory?.assets.map((asset) => (
           <button
             onClick={() => changeAsset(currentCategory.name, asset)}
